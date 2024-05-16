215ad0090fb8e15bd3c8d675a585aca4ef008de9
Alrik Olson <10505065+AlrikOlson@users.noreply.github.com>
2024-05-15 23:05:46 -0700

    awd

--- src/core/game.py
+++ src/core/game.py
@@ -113,54 +113,71 @@ class Game:
             (60, 179, 113),   # Forest
             (160, 82, 45),    # Mountain
             (255, 250, 250),  # Snow
-            (128, 128, 128)   # Rocky shoreline
+            (128, 128, 128),  # Rocky shoreline
         ]
+
+        def blend_colors(color1, color2, factor):
+            return tuple(
+                int(c1 * (1 - factor) + c2 * factor)
+                for c1, c2 in zip(color1, color2)
+            )
+
+        def smooth_elevation(elevations, x, y, size=3):
+            neighbors = elevations[max(0, x-size):min(elevations.shape[0], x+size+1), max(0, y-size):min(elevations.shape[1], y+size+1)]
+            return np.mean(neighbors)
 
         # Pad world_np to handle edge cases
         padded_world = np.pad(world_np, pad_width=1, mode='edge')
-        
+
         for x in range(1, world_np.shape[0] + 1):
             for y in range(1, world_np.shape[1] + 1):
                 elevation = padded_world[x, y]
+                smoothed_elevation = smooth_elevation(padded_world, x, y)
 
-                if elevation < deep_water_threshold:
+                if smoothed_elevation < deep_water_threshold:
                     color_array[x-1, y-1] = colors[0]  # Deep water
-                elif elevation < shallow_water_threshold:
+                elif smoothed_elevation < shallow_water_threshold:
                     # Gradient from shallow water to deep water
-                    blend_factor = (elevation - deep_water_threshold) / (shallow_water_threshold - deep_water_threshold)
-                    shallow_water_color = tuple(
-                        int(c1 * (1 - blend_factor) + c2 * blend_factor)
-                        for c1, c2 in zip(colors[0], colors[1])
-                    )
+                    blend_factor = (smoothed_elevation - deep_water_threshold) / (shallow_water_threshold - deep_water_threshold)
+                    shallow_water_color = blend_colors(colors[0], colors[1], blend_factor)
                     color_array[x-1, y-1] = shallow_water_color
-                elif elevation < beach_threshold:
+                elif smoothed_elevation < beach_threshold:
                     # Shoreline check
                     neighbors = padded_world[x-1:x+2, y-1:y+2]
                     if np.any(neighbors < shallow_water_threshold):
                         # Identify if it is an inner shoreline (lake)
                         is_lake = np.all(neighbors >= deep_water_threshold)
-                        if is_lake and np.random.rand() < 0.7:
-                            # More likely rocky for inner shores
+                        if is_lake and np.random.rand() < 0.3:  # Lower probability for rocky shorelines at this threshold
                             color_array[x-1, y-1] = colors[7]  # Rocky shoreline
                         else:
-                            # Vary the depth of the beach
-                            beach_depth = (elevation - shallow_water_threshold) / (beach_threshold - shallow_water_threshold)
-                            beach_color = tuple(int(c * (1 - beach_depth) + g * beach_depth) for c, g in zip(colors[2], colors[3]))
+                            # Vary the depth and size of the beach for outside shorelines
+                            beach_depth = (smoothed_elevation - shallow_water_threshold) / (beach_threshold - shallow_water_threshold)
+                            blend_factor = (smoothed_elevation - shallow_water_threshold) / (beach_threshold - shallow_water_threshold)
+                            beach_color = blend_colors(colors[2], colors[3], blend_factor)
                             color_array[x-1, y-1] = beach_color
                     else:
                         color_array[x-1, y-1] = colors[3]  # Grass (instead of sand if not near water)
-                elif elevation < grass_threshold:
-                    color_array[x-1, y-1] = colors[3]  # Grass
-                elif elevation < forest_threshold:
-                    color_array[x-1, y-1] = colors[4]  # Forest
-                elif elevation < mountain_threshold:
-                    # Add rocky shoreline near mountains
-                    if np.any(neighbors > mountain_threshold):
+                elif smoothed_elevation < grass_threshold:
+                    blend_factor = (smoothed_elevation - beach_threshold) / (grass_threshold - beach_threshold)
+                    grass_color = blend_colors(colors[3], colors[4], blend_factor)
+                    color_array[x-1, y-1] = grass_color
+                elif smoothed_elevation < forest_threshold:
+                    blend_factor = (smoothed_elevation - grass_threshold) / (forest_threshold - grass_threshold)
+                    forest_color = blend_colors(colors[4], colors[5], blend_factor)
+                    modified_forest_color = tuple(c + np.random.randint(-10, 10) for c in forest_color)  # Adding variation
+                    color_array[x-1, y-1] = modified_forest_color
+                elif smoothed_elevation < mountain_threshold:
+                    # Determine rocky shorelines for higher elevations, further from water
+                    if smoothed_elevation >= grass_threshold - 0.1 and np.random.rand() < 0.1:  # Lower probability for rocky shorelines
                         color_array[x-1, y-1] = colors[7]  # Rocky shoreline
                     else:
-                        color_array[x-1, y-1] = colors[5]  # Mountain
+                        mountain_color = colors[5]
+                        modified_mountain_color = tuple(c + np.random.randint(-10, 10) for c in mountain_color)  # Adding variation
+                        color_array[x-1, y-1] = modified_mountain_color
                 else:
-                    color_array[x-1, y-1] = colors[6]  # Snow
+                    snow_color = colors[6]
+                    modified_snow_color = tuple(c + np.random.randint(-10, 10) for c in snow_color)  # Adding variation
+                    color_array[x-1, y-1] = modified_snow_color
 
         print(f"Final color_array shape: {color_array.shape}, dtype: {color_array.dtype}")
 
